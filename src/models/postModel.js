7243c4601c735482039644e4fabe790bcbaa7d18
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-11-22 10:56:40 -0300

    aula 4

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -1,7 +1,12 @@
 import conectarAoBanco from "../config/dbConfig.js";//no beck precisa do .js (extensão do arquivo), diferentemente do front
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
     const db = conexao.db('imersao-beck-end') //representa o meu banco de dados
     const colecao = db.collection('posts') //representa a coleção específica dentro deste meu banco
     return colecao.find().toArray()
+}
+export async function criarPost(novoPost) {
+    const db = conexao.db('imersao-beck-end')
+    const colecao = db.collection('posts')
+    return colecao.insertOne(novoPost)
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,7 +1,28 @@
 import express from 'express';
-import { listarPosts } from '../controllers/postsControllers.js';
+import multer from 'multer';
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, 'uploads/');
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({dest: "./uploads", storage})
+//linux ou mac:
+//const upload = multer({dest: "./uploads", storage})
+import { listarPosts, postarNovoPost, uploadImagem } from '../controllers/postsControllers.js';
 const routes = app => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", postarNovoPost)
+    app.post("/posts/upload", upload.single("imagem"), uploadImagem)
 }
 export default routes;
+/*
+** 4 principais Verbos HTTP:
+** 1 - Ler (get)
+** 2 - Criar (post)
+** 3 - Atualizar (put)
+** 4 - Deletar (delete)
+ */
